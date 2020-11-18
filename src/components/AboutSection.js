aac7b8f6972251543a6e841fb0c0d470e2b6b444
devflamenbaum <bielflam.engpet@gmail.com>
2020-11-18 11:57:05 -0300

    12. Add Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import home1 from '../img/home1.png';
 // styles
-import styled from 'styled-components';
 import {About, Description, Hide, Image} from '../styles';
+// Framer Motion
+import {motion} from 'framer-motion';
 
 const AboutSection = () => {
+    
+    const titleAnim = {
+        hidden: { opacity: 0},
+        show: { opacity: 1, transition: { duration: 2}}
+    }
+
+    const container = {
+        hidden: { x: 300 },
+        show: { x: 0, transition: { duration: 0.75, ease: "easeOut", staggerChildren: 1, when: "beforeChildren"} }
+    }
+
     return(
         <About>
             <Description>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <Hide>
-                        <h2>We Work to make</h2>
+                        <motion.h2 variants={titleAnim}>We Work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                     <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
                     <button>Contact Us</button>
-                </div>
+                </motion.div>
             </Description>
             <Image>
                 <img src={home1} alt="guy with the camera"/>
