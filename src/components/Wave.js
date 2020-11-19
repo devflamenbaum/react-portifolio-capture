e1287374ee39278c2b47e24515148dd628815141
devflamenbaum <bielflam.engpet@gmail.com>
2020-11-19 12:55:19 -0300

    23. Mobile Friendly

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -23,6 +23,10 @@ const WaveSvg = styled.svg`
     position: absolute;
     left: 0;
     z-index: 1;
+    @media (max-width: 1300px){
+        top: 30%;
+        z-index: -1;
+    }
 `
 
 export default Wave;
